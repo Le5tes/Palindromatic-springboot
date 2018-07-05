374a052d77ab47436997a2c22401986327a86f67
Tim <Timwilliamson1337@gmail.com>
2018-07-05 20:54:35 +0100

    first commit: initialize spring boot repo

---
+++ src/main/java/com/palindromatic/palidromaticservice/PalidromaticServiceApplication.java
@@ -0,0 +1,12 @@
+package com.palindromatic.palidromaticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalidromaticServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalidromaticServiceApplication.class, args);
+	}
+}
