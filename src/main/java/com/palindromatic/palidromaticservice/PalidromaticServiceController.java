140f67dfad4537d1155fc6cccc1cb21f4b881a07
Tim <Timwilliamson1337@gmail.com>
2018-07-06 01:13:41 +0100

    implemented check/{value} in controller

--- src/main/java/com/palindromatic/palidromaticservice/PalidromaticServiceController.java
+++ src/main/java/com/palindromatic/palidromaticservice/PalidromaticServiceController.java
@@ -1,18 +1,24 @@
 package com.palindromatic.palidromaticservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class PalidromaticServiceController {
 
+    @Autowired
+    private PalidromaticServiceApplication palindromatic;
+
     @RequestMapping("/")
     public String index() {
         return "It's working!";
     }
 
-    @RequestMapping("/check/eevee")
-    public String check() {
-        return "true";
+    @RequestMapping("/check/{palindrome}")
+    public String check(@PathVariable String palindrome) {
+        return String.valueOf(palindromatic.check(palindrome));
     }
 
 }
